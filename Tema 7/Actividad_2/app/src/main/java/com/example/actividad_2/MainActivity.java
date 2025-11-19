18e7c7f11cfcc7f86533f85299f1bf976db5c8c0
JesusBenitezPitt <jesus.benitezpitt@iesvalleinclan.es>
2025-11-19 02:25:09 +0100

    Actividad 7.2 hecha

---
+++ Tema 7/Actividad_2/app/src/main/java/com/example/actividad_2/MainActivity.java
@@ -0,0 +1,32 @@
+package com.example.actividad_2;
+
+import android.os.Bundle;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        LayoutInflater inflater = getLayoutInflater();
+        View layout = inflater.inflate(R.layout.toast_custom, (ViewGroup) findViewById(R.id.toast_custom));
+
+        Toast toast = new Toast(MainActivity.this);
+        toast.setGravity(Gravity.CENTER, 0, 0);
+        toast.setDuration(Toast.LENGTH_SHORT);
+        toast.setView(layout);
+        toast.show();
+    }
+}

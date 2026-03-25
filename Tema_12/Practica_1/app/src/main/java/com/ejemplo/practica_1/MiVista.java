9d3a56aeb6e7a02d4b68a1697dea8aa9a0a96c68
Jesús Benítez Pitt <jesus.benitezpitt@iesvalleinclan.es>
2026-03-25 22:38:04 +0100

    Parte 1 Bloque 1 hecho

--- Tema_12/Practica_1/app/src/main/java/com/ejemplo/practica_1/MiVista.java
+++ Tema_12/Practica_1/app/src/main/java/com/ejemplo/practica_1/MiVista.java
@@ -4,33 +4,46 @@ import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
+import android.view.MotionEvent;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
 
 public class MiVista extends SurfaceView implements Runnable {
 
-    Thread gameThread;
-    boolean running = false;
-    SurfaceHolder holder;
-    Paint pincel;
+    private Thread gameThread;
+    private volatile boolean running = false;
+    private SurfaceHolder holder;
+    private Paint pincel;
+    private float bolaX = 400;
+    private float bolaY = 400;
+    private int radio = 50;
+    private float velocidadX = 10;
+    private float velocidadY = 15;
+
+    private int anchoPantalla;
+    private int altoPantalla;
 
     public MiVista(Context context) {
         super(context);
-
         holder = getHolder();
         pincel = new Paint();
         pincel.setColor(Color.RED);
+        pincel.setAntiAlias(true);
     }
 
     @Override
     public void run() {
         while (running) {
-            if (!holder.getSurface().isValid()) continue;
+            if (!holder.getSurface().isValid()) {
+                continue;
+            }
 
-            Canvas canvas = holder.lockCanvas();
-            canvas.drawColor(Color.WHITE);
-            canvas.drawCircle(400, 400, 100, pincel);
-            holder.unlockCanvasAndPost(canvas);
+            anchoPantalla = getWidth();
+            altoPantalla = getHeight();
+
+            actualizarMovimiento();
+
+            dibujar();
 
             try {
                 Thread.sleep(16);
@@ -38,6 +51,40 @@ public class MiVista extends SurfaceView implements Runnable {
                 e.printStackTrace();
             }
         }
+    }
+
+    private void actualizarMovimiento() {
+        bolaX += velocidadX;
+        bolaY += velocidadY;
+
+        if (bolaX + radio > anchoPantalla || bolaX - radio < 0) {
+            velocidadX = -velocidadX;
+        }
+
+        if (bolaY + radio > altoPantalla || bolaY - radio < 0) {
+            velocidadY = -velocidadY;
+        }
+    }
+
+    private void dibujar() {
+        Canvas canvas = holder.lockCanvas();
+        if (canvas != null) {
+            try {
+                canvas.drawColor(Color.WHITE);
+                canvas.drawCircle(bolaX, bolaY, radio, pincel);
+            } finally {
+                holder.unlockCanvasAndPost(canvas);
+            }
+        }
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        if (event.getAction() == MotionEvent.ACTION_DOWN) {
+            bolaX = event.getX();
+            bolaY = event.getY();
+        }
+        return true;
     }
 
     public void resume() {
@@ -55,4 +102,3 @@ public class MiVista extends SurfaceView implements Runnable {
         }
     }
 }
-
